dbdb85c55973d90a698beac5a93ec436cbb052d1
Stefan Rösch <stefan.roesch1985@gmail.com>
2017-06-22 14:47:16 +0200

    Add collection actions

--- client/src/actions/collections.js
+++ client/src/actions/collections.js
@@ -1,5 +1,76 @@
 import { createAction } from 'redux-actions';
-import { getAll } from '../sources/images_source';
+import { getAll, addImage, postNew, getImagesOfCollection } from '../sources/collections_source';
 
-export const getAllTags = createAction('GET_ALL_COLLECTIONS', () => getAll());
+export const getImagesForCollectionStart = createAction('GET_IMAGES_FOR_COLLECTION_START');
+export const getImagesForCollectionSucc = createAction('GET_IMAGES_FOR_COLLECTION_SUCC');
+export const getImagesForCollectionErr = createAction('GET_IMAGES_FOR_COLLECTION_ERR');
+
+export const getAllCollectionsStart = createAction('GET_ALL_COLLECTIONS_START');
+export const getAllCollectionsSucc = createAction('GET_ALL_COLLECTIONS_SUCC');
+export const getAllCollectionsErr = createAction('GET_ALL_COLLECTIONS_ERR');
+
+export const addImageToColStart = createAction('ADD_IMAGE_TO_COL_START');
+export const addImageToColSucc = createAction('ADD_IMAGE_TO_COL_SUCC');
+export const addImageToColErr = createAction('ADD_IMAGE_TO_COL_ERR');
+
+export const addNewColStart = createAction('ADD_NEW_COL_START');
+export const addNewColSucc = createAction('ADD_NEW_COL_SUCC');
+export const addNewColErr = createAction('ADD_NEW_COL_ERR');
+
 export const updateImage = createAction('UPDATE_COLLECTION');
+
+export const getImagesForCollection = id =>
+  (dispatch) => {
+    dispatch(getImagesForCollectionStart());
+    getImagesOfCollection(id)
+    .then((images) => {
+      dispatch(getImagesForCollectionSucc(images));
+      return Promise.resolve();
+    })
+    .catch((err) => {
+      dispatch(getImagesForCollectionErr(new Error(err)));
+      return Promise.resolve();
+    });
+  };
+
+export const getAllCollections = () =>
+  (dispatch) => {
+    dispatch(getAllCollectionsStart());
+    getAll()
+    .then((images) => {
+      dispatch(getAllCollectionsSucc(images));
+      return Promise.resolve();
+    })
+    .catch((err) => {
+      dispatch(getAllCollectionsErr(new Error(err)));
+      return Promise.resolve();
+    });
+  };
+
+export const addImageToCol = (collectionId, imageId) =>
+  (dispatch) => {
+    dispatch(addImageToColStart());
+    addImage(collectionId, imageId)
+    .then((images) => {
+      dispatch(addImageToColSucc(images));
+      return Promise.resolve();
+    })
+    .catch((err) => {
+      dispatch(addImageToColErr(new Error(err)));
+      return Promise.resolve();
+    });
+  };
+
+export const addNewCol = (name, description) =>
+  (dispatch) => {
+    dispatch(addNewColStart());
+    postNew(name, description)
+    .then((images) => {
+      dispatch(addNewColSucc(images));
+      return Promise.resolve();
+    })
+    .catch((err) => {
+      dispatch(addNewColErr(new Error(err)));
+      return Promise.resolve();
+    });
+  };
--- client/src/sources/collections_source.js
+++ client/src/sources/collections_source.js
@@ -10,5 +10,9 @@ export const getImagesOfCollection = id =>
   fetchWrapper(`collections/${id}/images`)
     .then(collections => collections);
 
-export const addTag = (collectionId, imageId) =>
+export const addImage = (collectionId, imageId) =>
   fetchWrapper(`collections/${collectionId}/images/${imageId}`, { method: 'POST' });
+
+export const postNew = (name, description) =>
+  fetchWrapper(`collections/new?name=${name}&description=${description}`, { method: 'POST' })
+    .then(collection => collection);
